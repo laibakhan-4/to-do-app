32c9ce45c371fe1983a60e37a7ac047718659c14
laiba-khan4 <laibakhan.intern@devsinc.com>
2024-07-15 20:17:03 +0500

    README commit

--- src/App.js
+++ src/App.js
@@ -5,40 +5,37 @@ import { BsCheckLg } from "react-icons/bs";
 
 function App() {
   const [isCompleteScreen,setIsCompleteScreen] = useState(false);
-  const [allTodos,setTodos] = useState([]);
+  const [allTodos,setTodos] = useState(() => {
+    return JSON.parse(localStorage.getItem ('todolist')) || [];
+  });
   const [newTitle,setNewTitle] = useState("");
   const [newDescription,setNewDescription] = useState("");
   const [completedTodos,setCompletedTodos] = useState([]);
   const [currentEdit,setCurrentEdit] = useState("");
   const [currentEditedItem,setCurrentEditedItem] = useState("");
 
-  //^ ADD btn function
   const handleAddTodo = ()=>{
     let newTodoItem = {
       title: newTitle,
       description: newDescription,
-    }; //*creates a new to-do item
+    }; 
     let updatedTodoArr=[...allTodos];
     updatedTodoArr.push (newTodoItem); 
     setTodos (updatedTodoArr);
-    //*adds it to the allTodos array
     localStorage.setItem ('todolist', JSON.stringify (updatedTodoArr));
-    //*stores in local storage
   };
 
-  //^DELETE  function
   const handleDeleteTodo = (index)=>{
     let reducedTodo = [...allTodos];
     reducedTodo.splice(index,1); 
-    //*removes the selected item from the allTodos array
     localStorage.setItem('todolist',JSON.stringify(reducedTodo));
     setTodos(reducedTodo);
-    //*updates the local storage
-
   };
 
-  //^CheckMark function
   const handleComplete = (index)=>{
+    Intl.DateTimeFormat(
+
+    ) 
     let now = new Date();
     let dow = now.getDay();
     let dd = now.getDate();
@@ -61,13 +58,10 @@ function App() {
     let updatedCompletedArr = [...completedTodos];
     updatedCompletedArr.push(filteredItem);
     setCompletedTodos(updatedCompletedArr);
-    //*moves the completed item from the allTodos array to the completedTodos array
     handleDeleteTodo(index);
     localStorage.setItem('completedTodos',JSON.stringify(updatedCompletedArr));
-    //*completed item is removed from the allTodos array & the local storage is updated
   };
 
-  //^DELETE function in completed tasks
   const handleDeleteCompletedTodo =(index)=>{
     let reducedTodo = [...completedTodos];
     reducedTodo.splice(index,1);
@@ -76,8 +70,6 @@ function App() {
   };
 
   useEffect (() => {
-    //* retrieves the todo list data from the browser's local storage
-    //* retrieved data is parsed from a JSON string back into a JavaScript object 
     let savedTodo = JSON.parse (localStorage.getItem ('todolist'));
     let savedCompletedTodo = JSON.parse (localStorage.getItem ('completedTodos'));
 
@@ -90,18 +82,17 @@ function App() {
     }
   }, []);
 
-  //*sets the currentEdit and currentEditedItem state variables to the selected item
   const handleEdit=(ind,item)=>{
     setCurrentEdit(ind);
     setCurrentEditedItem(item);
   };
 
-  //* updates the allTodos array with the modified item
   const handleUpdateTodo=()=>{
     let newToDo = [...allTodos];
     newToDo[currentEdit]=currentEditedItem;
     setTodos(newToDo);
-    setCurrentEdit(""); //*clears the currentEdit state variable
+    localStorage.setItem('todolist',JSON.stringify(newToDo));
+    setCurrentEdit(""); 
   };
 
   const handleUpdateTitle=(value)=>{
