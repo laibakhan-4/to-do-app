9771bdf4f2dada76afba376320be743b1086485f
laiba-khan4 <laibakhan.intern@devsinc.com>
2024-07-15 15:45:30 +0500

    Add functionality commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,91 @@
+import { useState } from 'react';
 import './App.css';
+import {AiOutlineDelete} from 'react-icons/ai';
+import { BsCheckLg } from "react-icons/bs";
 
 function App() {
-  return (
-    <div>
+  const [isCompleteScreen,setIsCompleteScreen] = useState(false);
+  const [allTodos,setTodos] = useState([]);
+  const [newTitle,setNewTitle] = useState("");
+  const [newDescription,setNewDescription] = useState("");
 
+  const handleAddTodo = ()=>{
+    let newTodoItem = {
+      title: newTitle,
+      description: newDescription,
+    };
+    let updatedTodoArr=[...allTodos];
+    updatedTodoArr.push (newTodoItem);
+    setTodos (updatedTodoArr);
+    localStorage.setItem ('todolist', JSON.stringify (updatedTodoArr));
+  };
+
+  return (
+    <div className='App'>
+      <h1>My Todos</h1>
+
+      <div className='todo-wrapper'>
+        <div className='todo-input'>
+          <div className='todo-input-item'>
+            <label>Title</label>
+            <input 
+            type="text" 
+            value={newTitle}
+            onChange={(e)=>setNewTitle(e.target.value)}
+            placeholder="What's the task Title"/>
+          </div>
+          <div className='todo-input-item'>
+            <label>Description</label>
+            <input 
+            type="text" 
+            value={newDescription}
+            onChange={(e)=>setNewDescription(e.target.value)}
+            placeholder="What's the task Description"/>
+          </div>
+          <div className='todo-input-item'>
+            <button 
+            type='button' 
+            onClick={handleAddTodo}
+            className='primaryBtn'>Add</button>
+          </div>
+        </div>
+
+        <div className='btn-area'>
+        <button
+            className={`secondaryBtn ${isCompleteScreen === false && 'active'}`}
+            onClick={() => setIsCompleteScreen (false)}
+          >
+            Todo
+          </button>
+          <button
+            className={`secondaryBtn ${isCompleteScreen === true && 'active'}`}
+            onClick={() => setIsCompleteScreen (true)}
+          >
+            Completed
+          </button>        </div>
+
+        <div className='todo-list'>
+          {allTodos.map((item,index)=>{
+            return(
+              <div className='todo-list-item' key={index}>
+            <div>
+              <h3>{item.title}</h3>
+              <p>{item.description}</p>
+            </div>
+
+            <div>
+              <AiOutlineDelete className='icon'/>
+              <BsCheckLg className='check-icon' />
+            </div>
+
+          </div>
+            )
+
+          })}
+
+        </div>
+
+      </div>
     </div>
   );
 }
